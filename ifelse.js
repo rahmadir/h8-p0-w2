a5c72d4f38c9b385171be3c8968853d58d5e337f
rahmadir <rahmadiridwan14@gmail.com>
2018-07-03 23:15:45 +0700

    Upload

---
+++ ifelse.js
@@ -0,0 +1,21 @@
+var nama = 'Nina';
+var peran = 'Kesatria';
+
+if (nama === '' && peran === ''){
+    console.log( 'Nama harus diisi!' )
+
+} if ( nama === 'Madi' && peran === '' ){
+    console.log('Halo Madi, Pilih peranmu untuk memulai game!')
+
+} if   (nama === 'Nina' && peran ==='Kesatria'){
+    console.log('Selamat datang di Dunia Proxtia, Nina')
+    console.log('Halo Ksatria Nina, kamu dapat menyerang dengan senjatamu!')
+
+} if (nama === 'Danu' && peran === 'Tabib'){
+    console.log('Selamat datang di Dunia Proxtia, Danu')
+    console.log('Halo Tabib Danu, kamu akan membantu temanmu yang terluka')
+
+} if (nama === 'Zero' && peran === 'Penyihir'){
+    console.log('Selamat datang di Dunia Proxtia, Zero')
+    console.log('Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!')
+}
